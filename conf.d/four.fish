bf44fa7c714d9bd208fda1922e31af9e125982bb
Tom Albrighton <thomas.albrighton@four.io>
2023-04-03 13:42:08 +0930

    Update

---
+++ conf.d/four.fish
@@ -0,0 +1,13 @@
+
+function _four_install --on-event four_install
+  set --export --global set PROJECTS_DIR /opt/vhosts;
+  # Set universal variables, create bindings, and other initialization logic.
+end
+
+function _four_update --on-event four_update
+  # Migrate resources, print warnings, and other update logic.
+end
+
+function _four_uninstall --on-event four_uninstall
+  # Erase "private" functions, variables, bindings, and other uninstall logic.
+end
